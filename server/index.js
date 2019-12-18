a359841b0e89958bc31d93a3914d2ac5e1f18c33
shanerbo <shanerbo@gmail.com>
2019-12-18 03:25:08 -0800

    move chat/join.js into corresponding folders

--- server/index.js
+++ server/index.js
@@ -9,5 +9,15 @@ const router = require('./router');
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+io.on('connection', (socket) => {
+      console.log('we have a new connection!!!!!!!');
+
+      socket.on('diconnect', () => {
+            console.log('User had left!!!');
+      })
+})
+
 app.use(router);
+
 server.listen(PORT, () => console.log(`server has started on port ${PORT}`));

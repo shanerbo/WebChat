c804a5afd510b782e0d7178dd6d3ff42c16961fc
shanerbo <shanerbo@gmail.com>
2019-12-17 20:45:27 -0800

    add dash board

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-import React, {Component} from 'react'
-
-class App extends Component{
-  render(){
-    return <div>I'm ready for changes</div>
-  }
+import Dashboard from './Dashboard';
+function App() {
+  return (
+    <div className="App">
+      <Dashboard/>
+    </div>
+  );
 }
 
 export default App;

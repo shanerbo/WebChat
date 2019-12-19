91234a2ba5fe34db961bc9852bfbc858d8055eb2
shanerbo <shanerbo@gmail.com>
2019-12-18 20:47:01 -0800

    add message and messages js files

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -4,6 +4,7 @@ import io from 'socket.io-client';
 import './Chat.css';
 import InfoBar from '../InfoBar/InfoBar.js';
 import Input from '../Input/Input.js';
+import Messages from '../Messages/Messages.js';
 let socket;
 
 const Chat = ({ location }) => {
@@ -54,6 +55,7 @@ const Chat = ({ location }) => {
             <div className="outerContainer">
                   <div className="container">
                         <InfoBar room={room} />
+                        <Messages messages={messages} name={name}></Messages>
                         <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
                   </div>
             </div>
--- server/index.js
+++ server/index.js
@@ -25,18 +25,26 @@ io.on('connection', (socket) => {
             socket.broadcast.to(user.room).emit("message", { user: "admin", text: `${user.name}, has joined` });
 
             socket.join(user.room);
+
+            io.to(user.room).emit("roomData", { room: user.room, users: getUsersInRoom(user.room) });
+
             callback();
       });
       socket.on("sendMessage", (message, callback) => {
             const user = getUser(socket.id);
 
             io.to(user.room).emit("message", { user: user.name, text: message });
+            io.to(user.room).emit("roomData", { room: user.room, users: getUsersInRoom(user.room) });
 
             callback();
       })
 
       socket.on("diconnect", () => {
-            console.log('User had left!!!');
+            const user = removeUser(socoet.id);
+
+            if (user) {
+                  io.to(user.room).emit("message", { user: "admin", text: `${user.name} has left` })
+            }
       })
 })
 
